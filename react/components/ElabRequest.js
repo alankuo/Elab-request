7faa2fab56b3caaafd01ce2b53f43a21a1f5fabc
Kiki-Jiang <xij035@ucsd.edu>
2017-02-26 04:39:43 -0800

    Basic editing mode

--- react/components/ElabRequest.js
+++ react/components/ElabRequest.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { database } from './../../database/database_init';
 import { Button, ButtonGroup, Glyphicon } from 'react-bootstrap';
-import Spinner from 'react-spinkit';
 
 /**
 ElabRequest - to be displayed on the side
@@ -31,18 +30,32 @@ class ElabRequest extends React.Component {
         });
 
         // Bind all functions so they can refer to "this" correctly
-        /*this.handleEdit = this.handleEdit.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
+        this.toggleEditing = this.toggleEditing.bind(this);
+        this.onChange = this.onChange.bind(this);
+        this.requestsList = this.requestsList.bind(this);
+        /*this.handleSubmit = this.handleSubmit.bind(this);
         this.updateQuestionFromDB = this.updateQuestionFromDB.bind(this);*/
 
 
     }
 
-    /*handleEdit(event) {
+    handleEdit(event) {
         this.setState({question: event.target.value});
     }
 
-    handleSubmit(event) {
+    onChange(text) {
+        // in order to render the updated text,
+        // you need to pass it as a prop to contentEditable.
+        // This gives you increased flexibility.
+        this.setState({answer: text})
+    }
+
+    toggleEditing() {
+        this.setState({ answer_editing: !this.state.answer_editing });
+    }
+
+    /*handleSubmit(event) {
         alert('The question has been submitted: ');
         event.preventDefault();
         var newPostKey = database.ref().child('elab-request').push().key;
@@ -70,21 +83,73 @@ class ElabRequest extends React.Component {
         });
     }*/
 
+    // after finishing the database part, add request as the parameter
+    requestsList() {
+        /*var question = requests[request].question;
+         var answer = requests[request].answer;*/
+        if (this.state.answer_editing) {
+            return (
+                <div className="request">
+                    <div className="request-question">
+                        <text>Questions got from database</text>
+                        <br />
+                    </div>
+
+                    <div className="request-answer" style={{borderStyle: 'rounded'}}>
+
+                        <text className="request-list-answer">
+                            <input
+                                onChange={ this.handleEdit }
+                                type="text"
+                                className="form-control"
+                                defaultValue="Answers"
+                            />
+                        </text>
+                        <Button bsStyle="primary" onClick={this.toggleEditing}>
+                            Submit
+                        </Button>
+                        <Button bsStyle="primary" onClick={this.toggleEditing}>
+                            Cancel
+                        </Button>
+                    </div>
+                </div>);
+        } else {
+            return (
+                <div className="request">
+                    <div className="request-question">
+                        <text>Questions got from database</text>
+                        <br />
+                    </div>
+
+                    <div className="request-answer" style={{borderStyle: 'rounded'}}>
+                        <text onClick={ this.toggleEditing } className="request-list-answer">
+                            Answers
+                        </text>
+                    </div>
+                </div>);
+        }
+    }
+
+
     render () {
         var requests = this.requests;
 
-        var requestsList = function (request) {
-            var question = requests[request].question;
-            var answer = requests[request].answer;
-            return (
-                <div className="request" key={request}>
-                    <div>
-                        
 
-                    </div>
-                </div>
-            )
-        }
+
+
+
+        /*var enableEditing = function(){
+            // set your contenteditable field into editing mode.
+            this.setState({ answer_editing: true });
+        }*/
+
+
+
+
+        // after finishing the database part, add following code to the request-list part
+        /*{this.requests.map( ( request ) => {
+         return requestsList( request );
+         })}*/
 
         return (
             <div className="container">
@@ -108,15 +173,18 @@ class ElabRequest extends React.Component {
                     </form>
                 </div>
 
+                <br />
 
                 <div className="request-history">
-                    <ul className="request-list">
-                        {this.requests.map(requestsList)}
-                    </ul>
+                    <div className="request-list">
+                        {this.requestsList()}
+                    </div>
                 </div>
+
             </div>
         );
     }
 }
+
 
 export default ElabRequest;
